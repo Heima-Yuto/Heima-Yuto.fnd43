ac845bf90e18994dfd3d0a76cc44a6c1c8f48fc7
Heima-Yuto <yuto_heima@mail.toyota.co.jp>
2026-01-21 14:35:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // 変数置き場
 const stake = document.getElementById("stake");
 const cardNumArr = [
-    2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A",
+  2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A",
 ]
 const myNum = document.getElementById("myCardNum");
 const dealerNum = document.getElementById("dealerCardNum");
@@ -13,7 +13,7 @@ const headerArea = document.getElementsByClassName("header");
 const haveMoney = document.getElementById("haveMoney");
 const highLowSelectEl = document.getElementsByClassName("highLowSelect");
 const selectEl = document.getElementsByName("select");
-const toHalfnums = text => text.replace(/[０-９]/g, m=>'０１２３４５６７８９'.indexOf(m));
+const toHalfnums = text => text.replace(/[０-９]/g, m => '０１２３４５６７８９'.indexOf(m));
 
 let dealerCardNum = 0;
 let myCardNum = 0;
@@ -23,97 +23,123 @@ let checkedValue = "";
 
 // カードの数値生成
 function gameStartFirst() {
-    if (Number.isNaN(Number(toHalfnums(stake.value))) || Number(toHalfnums(stake.value)) === 0) {
-        return window.alert("掛け金を入力してください");
-    }
-    dealerNumGenerate();
-    popupSelecter();
-    document.getElementById("resultArea").style.visibility = "hidden";
-    document.getElementById("money100").style.visibility = "hidden";
+  if (Number.isNaN(Number(toHalfnums(stake.value))) || Number(toHalfnums(stake.value)) === 0) {
+    return window.alert("掛け金を入力してください");
+  }
+  if (Number(toHalfnums(stake.value)) > Number(haveMoney.textContent)) {
+    return window.alert("所持金以上の金額は掛けられません");
+  }
+  if (!Number.isInteger(Number(toHalfnums(stake.value)))) {
+    return window.alert("小数点以下の入力は受け付けません")
+  }
+  dealerNumGenerate();
+  popupSelecter();
+  document.getElementById("resultArea").style.visibility = "hidden";
+  document.getElementById("money100").style.visibility = "hidden";
 }
 
 function gameStartSecond() {
-    Array.from(selectEl).filter((element) => {
-        if (element.checked) {
-            return checkedValue = element.value;
-        }
-    })
-    Object.values(highLowSelectEl).map((element) => element.style.visibility = "hidden")
-    myNumGenerate();
-    judgement();
-    document.getElementById("resultArea").style.visibility = "visible";
+  Array.from(selectEl).filter((element) => {
+    if (element.checked) {
+      return checkedValue = element.value;
+    }
+  })
+  Object.values(highLowSelectEl).map((element) => element.style.visibility = "hidden")
+  myNumGenerate();
+  judgement();
+  document.getElementById("resultArea").style.visibility = "visible";
 }
 
 function endOfGame() {
-    Object.values(headerArea).map((element) => element.style.visibility = "visible");
-    popupArea.style.visibility = "hidden";
-    myNum.textContent = "";
-    resultsDisplay.textContent = "";
-    Object.values(highLowSelectEl).map((element) => element.style.visibility = "hidden")
-    document.getElementById("resultArea").style.visibility = "hidden";
-    document.getElementById("money100").style.visibility = "visible"
+  Object.values(headerArea).map((element) => element.style.visibility = "visible");
+  popupArea.style.visibility = "hidden";
+  myNum.textContent = "";
+  resultsDisplay.textContent = "";
+  Object.values(highLowSelectEl).map((element) => element.style.visibility = "hidden")
+  document.getElementById("resultArea").style.visibility = "hidden";
+  document.getElementById("money100").style.visibility = "visible"
+  if (Number.isNaN(Number(haveMoney.textContent)) || Number(haveMoney.textContent) < 0) {
+    haveMoney.textContent = "コインを補充してください"
+  }
+
 }
+
+function endOfGame2() {
+  Object.values(headerArea).map((element) => element.style.visibility = "visible");
+  popupArea.style.visibility = "hidden";
+  myNum.textContent = "";
+  resultsDisplay.textContent = "";
+  Object.values(highLowSelectEl).map((element) => element.style.visibility = "hidden")
+  document.getElementById("resultArea").style.visibility = "hidden";
+  document.getElementById("money100").style.visibility = "visible"
+  stake.value = "";
+  if (Number.isNaN(Number(haveMoney.textContent)) || Number(haveMoney.textContent) < 0) {
+    haveMoney.textContent = "コインを補充してください"
+  }
+
+}
+
 
 
 // 以下ヘルパー関数置き場
 function dealerNumGenerate() {
-    dealerCardNum = (Math.floor(Math.random() * 13)); // 0~12のランダムな値
-    dealerNum.textContent = conversionNum(dealerCardNum);
+  dealerCardNum = (Math.floor(Math.random() * 13)); // 0~12のランダムな値
+  dealerNum.textContent = conversionNum(dealerCardNum);
 }
 
 function myNumGenerate() {
-    myCardNum = (Math.floor(Math.random() * 13)); // 0~12のランダムな値
-    myNum.textContent = conversionNum(myCardNum);
+  myCardNum = (Math.floor(Math.random() * 13)); // 0~12のランダムな値
+  myNum.textContent = conversionNum(myCardNum);
 }
 
 // 変換関数
 function conversionNum(randomNum) {
-    return cardNumArr[randomNum];
+  return cardNumArr[randomNum];
 }
 
 function stakeDelete() {
-    console.log("stakeDelete");
-    stake.value = "";
+  console.log("stakeDelete");
+  stake.value = "";
 }
 
 function popupSelecter() {
-    popupArea.style.visibility = "visible";
-    Object.values(highLowSelectEl).map((element) => element.style.visibility = "visible")
-    Object.values(headerArea).map((element) => element.style.visibility = "hidden");
+  popupArea.style.visibility = "visible";
+  Object.values(highLowSelectEl).map((element) => element.style.visibility = "visible")
+  Object.values(headerArea).map((element) => element.style.visibility = "hidden");
 }
 
 function addMoney100() {
-    haveMoney.textContent = 100;
+  haveMoney.textContent = 100;
 }
 
 function judgement() {
-    if (checkedValue === "high") {
-        return highJudge();
-    } else if (checkedValue === "low") {
-        return lowJudge();
-    }
+  if (checkedValue === "high") {
+    return highJudge();
+  } else if (checkedValue === "low") {
+    return lowJudge();
+  }
 }
 
 function highJudge() {
-    if (myCardNum > dealerCardNum) {
-        resultsDisplay.textContent = "あなたの勝ちです！！";
-        haveMoney.textContent = Number(haveMoney.textContent) + Number(toHalfnums(stake.value));
-    } else if (myCardNum < dealerCardNum) {
-        resultsDisplay.textContent = "あなたの負けです、、";
-        haveMoney.textContent = Number(haveMoney.textContent) - Number(toHalfnums(stake.value));
-    } else {
-        resultsDisplay.textContent = "引き分けですね";
-    }
+  if (myCardNum > dealerCardNum) {
+    resultsDisplay.textContent = "あなたの勝ちです！！";
+    haveMoney.textContent = Number(haveMoney.textContent) + Number(toHalfnums(stake.value));
+  } else if (myCardNum < dealerCardNum) {
+    resultsDisplay.textContent = "あなたの負けです、、";
+    haveMoney.textContent = Number(haveMoney.textContent) - Number(toHalfnums(stake.value));
+  } else {
+    resultsDisplay.textContent = "引き分けですね";
+  }
 }
 
 function lowJudge() {
-    if (myCardNum < dealerCardNum) {
-        resultsDisplay.textContent = "あなたの勝ちです！！";
-        haveMoney.textContent = Number(haveMoney.textContent) + Number(toHalfnums(stake.value));
-    } else if (myCardNum > dealerCardNum) {
-        resultsDisplay.textContent = "あなたの負けです、、";
-        haveMoney.textContent = Number(haveMoney.textContent) - Number(toHalfnums(stake.value));
-    } else {
-        resultsDisplay.textContent = "引き分けですね";
-    }
+  if (myCardNum < dealerCardNum) {
+    resultsDisplay.textContent = "あなたの勝ちです！！";
+    haveMoney.textContent = Number(haveMoney.textContent) + Number(toHalfnums(stake.value));
+  } else if (myCardNum > dealerCardNum) {
+    resultsDisplay.textContent = "あなたの負けです、、";
+    haveMoney.textContent = Number(haveMoney.textContent) - Number(toHalfnums(stake.value));
+  } else {
+    resultsDisplay.textContent = "引き分けですね";
+  }
 }
